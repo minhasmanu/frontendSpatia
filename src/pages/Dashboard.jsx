8bfe0658ba234dce45d27c605f6ad87d70e30ffd
minhasmanu <manuminhas33@gmail.com>
2026-03-02 05:52:22 +0530

    Update API endpoint URL to https://spatia.co.in/

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -69,7 +69,7 @@ export default function Dashboard() {
       console.log("Uploading file:", fileToUpload.name, fileToUpload.size, "bytes");
 
       const res = await axios.post(
-        "http://51.21.148.216:8081/",
+        "https://spatia.co.in/",
         formData,
         { 
           responseType: "blob"
